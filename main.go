1bf708bae3c34d06c4b031624be79783446b029b
Bailey Miles <baileyleah542@gmail.com>
2025-11-18 11:15:49 +0000

    Serve html files and other files in the assets

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -23,9 +26,39 @@ func createEndpoints(router *gin.Engine) {
 
 func main() {
 	router := gin.Default()
+
 	createEndpoints(router)
 
-	//router.Static("/", "./public") - Fix
+	router.Static("/assets", "./assets")
+
+	router.NoRoute(func(c *gin.Context) {
+		if c.Request.Method == "GET" {
+			path := filepath.Join("./public", c.Request.URL.Path)
+
+			if info, err := os.Stat(path); err == nil && !info.IsDir() {
+				c.File(path)
+				return
+			}
+
+			if filepath.Ext(path) == "" {
+				htmlPath := path + ".html"
+				if _, err := os.Stat(htmlPath); err == nil {
+					c.File(htmlPath)
+					return
+				}
+			}
+
+			if info, err := os.Stat(path); err == nil && info.IsDir() {
+				indexPath := filepath.Join(path, "index.html")
+				if _, err := os.Stat(indexPath); err == nil {
+					c.File(indexPath)
+					return
+				}
+			}
+
+			c.File("./public/404.html")
+		}
+	})
 
 	router.Run(":8080")
 }
