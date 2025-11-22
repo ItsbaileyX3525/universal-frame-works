5b4880584455b08c1147f7505e8d65a387fe3263
Bailey Miles <baileyleah542@gmail.com>
2025-11-22 15:45:16 +0000

    implement tls/certs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -25,11 +26,22 @@ func main() {
 	dbPass = os.Getenv("DB_PASS")
 	websiteURL = os.Getenv("WEBSITE_NAME")
 	var PORT string = os.Getenv("PORT")
+	var useTLS bool = true
 
 	var router *gin.Engine = gin.Default()
 
 	createEndpoints(router)
 	renderHTML(router)
 
-	router.Run(fmt.Sprintf(":%s", PORT))
+	_, fileTest := os.Stat("ssl/cert.pem")
+
+	if errors.Is(fileTest, os.ErrNotExist) {
+		useTLS = false
+	}
+
+	if useTLS {
+		router.RunTLS(fmt.Sprintf(":%s", PORT), "ssl/cert.pem", "ssl/key.pem")
+	} else {
+		router.Run(fmt.Sprintf(":%s", PORT))
+	}
 }
